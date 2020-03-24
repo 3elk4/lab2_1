b2654c68cf86037c93a9697ef4001c622d798dd9
belka <izaa1109@gmail.com>
2020-03-24 16:11:59 +0100

    testy_v2
    + negative sequence test
    + value not in duplicateSequence

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -1,9 +1,12 @@
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.is;
 
 public class BinarySearchTest {
     static private BinarySearch binarySearch;
@@ -12,6 +15,7 @@ public class BinarySearchTest {
     static private int[] emptySequence;
     static private int[] decreasingSequence;
     static private int[] duplicateSequence;
+    static private int[] negativeSequence;
 
     @BeforeAll
     static void init(){
@@ -21,59 +25,78 @@ public class BinarySearchTest {
         emptySequence = new int[]{};
         decreasingSequence = new int[]{5, 4, 3, 2, 1};
         duplicateSequence = new int[]{5, 5, 5, 5, 5};
+        negativeSequence = new int[]{-5, -4, -3, -2, -1};
     }
 
     @Test
     void shouldBeInSequence(){
         int key = 1;
         SearchResult result = binarySearch.search(key, shortSequence);
-        MatcherAssert.assertThat("Is in sequence", result.isFound());
-        MatcherAssert.assertThat("Key is 1", shortSequence[result.getPosition()] == key);
+        MatcherAssert.assertThat("Is not in sequence", result.isFound(), is(true));
+        MatcherAssert.assertThat("Key is not 1", shortSequence[result.getPosition()], is(key));
     }
 
     @Test
     void shouldBeNotInSequence(){
         int key = 2;
         SearchResult result = binarySearch.search(key, shortSequence);
-        MatcherAssert.assertThat("Is not in sequence", !result.isFound());
-        MatcherAssert.assertThat("Key is not 2", result.getPosition() == -1);
+        MatcherAssert.assertThat("Is in sequence", result.isFound(), is(false));
+        MatcherAssert.assertThat("Key is 2", result.getPosition(), is(-1));
     }
 
     @Test
     void shouldBeFirstElementInSequence(){
         int key = 1;
         SearchResult result = binarySearch.search(key, longSequence);
-        MatcherAssert.assertThat("Is in sequence", result.isFound());
-        MatcherAssert.assertThat("Key is first element", result.getPosition() == 0);
+        MatcherAssert.assertThat("Is not in sequence", result.isFound(), is(true));
+        MatcherAssert.assertThat("Key is not first element", result.getPosition(), is(0));
     }
 
     @Test
     void shouldBeLastElementInSequence(){
         int key = 5;
         SearchResult result = binarySearch.search(key, longSequence);
-        MatcherAssert.assertThat("Is in sequence", result.isFound());
-        MatcherAssert.assertThat("Key is last element", result.getPosition() == longSequence.length-1);
+        MatcherAssert.assertThat("Is not in sequence", result.isFound(), is(true));
+        MatcherAssert.assertThat("Key is not last element", result.getPosition(), is(longSequence.length-1));
     }
 
     @Test
     void shouldBeMidElementInSequence(){
         int key = 3;
         SearchResult result = binarySearch.search(key,longSequence);
-        MatcherAssert.assertThat("Is in sequence", result.isFound());
-        MatcherAssert.assertThat("Key is mid element", result.getPosition() == longSequence.length/2);
+        MatcherAssert.assertThat("Is not in sequence", result.isFound(), is(true));
+        MatcherAssert.assertThat("Key is not mid element", result.getPosition(), is(longSequence.length/2));
     }
 
     @Test
     void shouldBeNotInSequence2(){
         int key = 0;
         SearchResult result = binarySearch.search(key, longSequence);
-        MatcherAssert.assertThat("Is not in sequence", !result.isFound());
-        MatcherAssert.assertThat("Key is not 2", result.getPosition() == -1);
+        MatcherAssert.assertThat("Is in sequence", result.isFound(), is(false));
+        MatcherAssert.assertThat("Key is 0", result.getPosition(), is(-1));
     }
 
     @Test
     void shouldThrowIllegalArgumentException(){
         Assertions.assertThrows(IllegalArgumentException.class, () -> binarySearch.search(5, emptySequence));
     }
+
+    @Test
+    void shouldBeNotInSequence3(){
+        int key = 2;
+        SearchResult result = binarySearch.search(key, duplicateSequence);
+        MatcherAssert.assertThat("Is in sequence", result.isFound(), is(false));
+        MatcherAssert.assertThat("Key is 2", result.getPosition(), is(-1));
+    }
+
+    @Test
+    void shouldBeInSequence2(){
+        int key = -1;
+        SearchResult result = binarySearch.search(key, shortSequence);
+        MatcherAssert.assertThat("Is not in sequence", result.isFound(), is(true));
+        MatcherAssert.assertThat("Key is not -1", shortSequence[result.getPosition()], is(key));
+    }
+
+
 
 }
